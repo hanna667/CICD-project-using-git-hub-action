da3365a9d8da59e4f07d6ba99c684a91154b0dfa
hanna667 <hannapaikkatt1234@gmail.com>
2025-07-24 15:57:58 +0530

    Create pom.java

---
+++ pom.java
@@ -0,0 +1,20 @@
+<project xmlns="http://maven.apache.org/POM/4.0.0" 
+         xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+         xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 
+         http://maven.apache.org/xsd/maven-4.0.0.xsd">
+         
+  <modelVersion>4.0.0</modelVersion>
+  <groupId>com.example</groupId>
+  <artifactId>myapp90</artifactId>
+  <version>1.0.0</version>
+  <packaging>jar</packaging>
+
+  <name>MyApp1</name>
+
+  <distributionManagement>
+    <repository>
+      <id>hanna667</id>
+      <url>https://maven.pkg.github.com/hanna667/CICD-project-using-git-hub-action</url>
+    </repository>
+  </distributionManagement>
+</project>
